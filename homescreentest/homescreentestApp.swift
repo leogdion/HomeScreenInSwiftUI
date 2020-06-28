790c9c39d609ca6dd345f9c6fab6c0e99c7d1a36
Leo Dion <leogdion@brightdigit.com>
2020-06-28 18:25:52 -0400

    Initial Commit

---
+++ homescreentest/homescreentestApp.swift
@@ -0,0 +1,17 @@
+//
+//  homescreentestApp.swift
+//  homescreentest
+//
+//  Created by Leo Dion on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct homescreentestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
